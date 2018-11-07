289ba72757a6cd6476102c9cc5aaa2152852722b
vrait35 <vrait35@mail.ru>
2018-11-08 02:21:20 +0600

    XmlCreate

---
+++ XmlHelpDescriptionStudent/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlHelpDescriptionStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xml = new XmlDocument();
+            XmlNode rootElement = xml.CreateElement("Shag");
+           
+                var studentElement = xml.CreateElement("student");
+                studentElement.SetAttribute("Name", "Anuar");
+                studentElement.SetAttribute("age", "20");
+                studentElement.SetAttribute("gender","male" );           
+                rootElement.AppendChild(studentElement);
+            var node = xml.CreateElement("raiting");
+            node.InnerText = "12";
+            studentElement.AppendChild(node);
+            xml.AppendChild(rootElement);
+            xml.Save("data.txt");
+        }
+    }
+}
---
+++ XmlHelpDescriptionStudent/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("XmlHelpDescriptionStudent")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XmlHelpDescriptionStudent")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8f4e0fd2-f253-46d1-b5b1-95c1eba5ab35")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
